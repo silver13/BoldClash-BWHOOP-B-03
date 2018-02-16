4c7485e2c4fa01919368ef58964ae9679e2a344c
NotFastEnuf <35885341+NotFastEnuf@users.noreply.github.com>
2018-02-16 12:46:37 -0500

    Update horizon, add racemode horizon, restructure flight modes

--- Silverware/src/control.c
+++ Silverware/src/control.c
@@ -642,39 +642,107 @@ if (aux[LEVELMODE]&&!acro_override){
 	yawerror[1] = - GEstG[0] * yawrate;
 	yawerror[2] = GEstG[2] * yawrate;
 	
-	if (aux[RACEMODE]){ //racemode overrites standard level behavior
-		// roll is leveled and mixed with error from yaw gravity vector
-		int i = 0 ;
-		angleerror[i] = errorvect[i] ; 
-		error[i] = apid(i) + yawerror[i] - gyro[i];
-		// pitch is acro - ***need to test mixing with yaw gravity vector error
-		error[1] = rxcopy[1] * (float) MAX_RATE * DEGTORAD - gyro[1];
-		// yaw
-		error[2] = yawerror[2] - gyro[2];
-	}else{ //standard leveling behavior is either horizon or level
-		if  (aux[HORIZON]){//horizon mode				
+	
+	//*************************************************************************
+	//horizon modes tuning variables
+	//*************************************************************************
+	// 1.0 is pure angle based transition, 0.0 is pure stick defelction based transition, values inbetween are a mix of both.  Adjust from 0 to 1
+	float HORIZON_SLIDER = 0.3f;
+	//leveling transitions into acro below this angle - above this angle is all acro.  DO NOT SET ABOVE 85 DEGREES!
+	float HORIZON_ANGLE_TRANSITION = 55.0f;
+	//leveling transitions into acro below this stick position - beyond this stick position is all acro. Adjust from 0 to 1
+	float HORIZON_STICK_TRANSITION = 0.95f;
+	//*************************************************************************
+	//*************************************************************************
+	
+	
+	if (aux[RACEMODE] && !aux[HORIZON]){ //racemode with angle behavior on roll ais
+			if (GEstG[2] < 0 ){ // acro on roll and pitch when inverted
+					error[0] = rxcopy[0] * (float) MAX_RATE * DEGTORAD - gyro[0];
+					error[1] = rxcopy[1] * (float) MAX_RATE * DEGTORAD - gyro[1];
+			}else{
+					//roll is leveled to max angle limit
+					angleerror[0] = errorvect[0] ; 
+					error[0] = apid(0) + yawerror[0] - gyro[0];
+					//pitch is acro 
+					error[1] = rxcopy[1] * (float) MAX_RATE * DEGTORAD - gyro[1];}
+			// yaw
+			error[2] = yawerror[2] - gyro[2];
+		
+	}else if(aux[RACEMODE] && aux[HORIZON]){	//racemode with horizon behavior on roll axis	
+			float inclinationRoll	= attitude[0];
+			float inclinationPitch = attitude[1];
+			float inclinationMax;
+			if (fabsf(inclinationRoll) >= fabsf(inclinationPitch)){
+					inclinationMax = fabsf(inclinationRoll);
+			}else{
+					inclinationMax = fabsf(inclinationPitch);}
+			float angleFade;
+			// constrains acroFade variable between 0 and 1
+			if (inclinationMax <= HORIZON_ANGLE_TRANSITION){
+					angleFade = inclinationMax/HORIZON_ANGLE_TRANSITION;
+			}else{
+					angleFade = 1;}
+			float stickFade;
+			float deflection = fabsf(rxcopy[0]);
+			if (deflection <= HORIZON_STICK_TRANSITION){
+					stickFade = deflection/HORIZON_STICK_TRANSITION;
+			}else{
+					stickFade = 1;}
+			float fade = (stickFade *(1-HORIZON_SLIDER))+(HORIZON_SLIDER * angleFade);
+			// apply acro to roll for inverted behavior
+			if (GEstG[2] < 0 ){
+					error[0] = rxcopy[0] * (float) MAX_RATE * DEGTORAD - gyro[0];
+					error[1] = rxcopy[1] * (float) MAX_RATE * DEGTORAD - gyro[1];
+			}else{ // apply a transitioning mix of acro and level behavior inside of stick HORIZON_TRANSITION point and full acro beyond stick HORIZON_TRANSITION point					
+					angleerror[0] = errorvect[0] ;
+					// roll angle strength fades out as sticks approach HORIZON_TRANSITION while acro stength fades in according to value of acroFade factor
+					error[0] = ((apid(0) + yawerror[0] - gyro[0]) * (1 - fade)) + (fade * (rxcopy[0] * (float) MAX_RATE * DEGTORAD - gyro[0]));
+					//pitch is acro
+					error[1] = rxcopy[1] * (float) MAX_RATE * DEGTORAD - gyro[1];
+			}
+	
+			// yaw
+			error[2] = yawerror[2]  - gyro[2];  
+		
+	}else if(!aux[RACEMODE] && aux[HORIZON]){ //horizon overrites standard level behavior	
 			//pitch and roll
-			for ( int i = 0 ; i <=1; i++){
-					float deflection = fabsf(rxcopy[i]);
-					float acroFade;
-					// constrains acroFade variable between 0 and 1
-					if (deflection <= HORIZON_TRANSITION){
-						acroFade = deflection/HORIZON_TRANSITION;
+			for ( int i = 0 ; i <=1; i++){	
+			  	float inclinationRoll	= attitude[0];
+					float inclinationPitch = attitude[1];
+					float inclinationMax;
+					if (fabsf(inclinationRoll) >= fabsf(inclinationPitch)){
+						inclinationMax = fabsf(inclinationRoll);
 					}else{
-						acroFade = 1;
+						inclinationMax = fabsf(inclinationPitch);}
+					float angleFade;
+					// constrains acroFade variable between 0 and 1
+					if (inclinationMax <= HORIZON_ANGLE_TRANSITION){
+						angleFade = inclinationMax/HORIZON_ANGLE_TRANSITION;
+					}else{
+						angleFade = 1;
 					}
+					float stickFade;
+					float deflection = fabsf(rxcopy[i]);
+					if (deflection <= HORIZON_STICK_TRANSITION){
+						stickFade = deflection/HORIZON_STICK_TRANSITION;
+					}else{
+						stickFade = 1;
+					}
+					float fade = (stickFade *(1-HORIZON_SLIDER))+(HORIZON_SLIDER * angleFade);
 					// apply acro to roll and pitch sticks for inverted behavior
 					if (GEstG[2] < 0 ){
 						error[i] = rxcopy[i] * (float) MAX_RATE * DEGTORAD - gyro[i];
 					}else{ // apply a transitioning mix of acro and level behavior inside of stick HORIZON_TRANSITION point and full acro beyond stick HORIZON_TRANSITION point					
 						angleerror[i] = errorvect[i] ;
 						//  angle strength fades out as sticks approach HORIZON_TRANSITION while acro stength fades in according to value of acroFade factor
-						error[i] = ((apid(i) + yawerror[i] - gyro[i]) * (1-acroFade)) + (acroFade * (rxcopy[i] * (float) MAX_RATE * DEGTORAD - gyro[i]));
+						error[i] = ((apid(i) + yawerror[i] - gyro[i]) * (1 - fade)) + (fade * (rxcopy[i] * (float) MAX_RATE * DEGTORAD - gyro[i]));
 					}
 			}
 			// yaw
-			error[2] = yawerror[2]  - gyro[2];  				
-		}else{ //standard level mode
+			error[2] = yawerror[2]  - gyro[2];  
+			
+	}else{ //standard level mode
 	    // pitch and roll
 			for ( int i = 0 ; i <=1; i++){
 					angleerror[i] = errorvect[i] ;    
@@ -683,8 +751,7 @@ if (aux[LEVELMODE]&&!acro_override){
       // yaw
 			error[2] = yawerror[2]  - gyro[2];
 		}
-	}
-}    
+	}  
 	
 }
 
